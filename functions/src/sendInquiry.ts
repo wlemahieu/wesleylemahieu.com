6d5cda6947665dbb9b4752e13bd879eda1e4271e
Wesley LeMahieu <wesleylemahieu@gmail.com>
2023-02-21 11:14:41 -0800

    drop enforceAppCheck

--- functions/src/sendInquiry.ts
+++ functions/src/sendInquiry.ts
@@ -6,7 +6,6 @@ import validateAppCheckVerified from './validators/appCheckVerified';
 const sendInquiry = functions
   .region('us-central1')
   .runWith({
-    enforceAppCheck: true,
     secrets: ['SENDGRID_API_KEY'],
     memory: '256MB',
   })
