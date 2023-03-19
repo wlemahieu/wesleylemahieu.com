18d2d44f1f44b1d62e6aa783fd3d901c06854b0a
Wesley LeMahieu <wesleylemahieu@gmail.com>
2023-03-18 19:31:13 -0700

    force use prod sendEmail cf url

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const functionURL =
-  process.env.MODE === 'development'
-    ? 'http://127.0.0.1:5001/wesleylemahieu-com/us-central1/sendEmail'
-    : 'https://us-central1-wesleylemahieu-com.cloudfunctions.net/sendEmail';
+const functionURL = 'https://us-central1-wesleylemahieu-com.cloudfunctions.net/sendEmail';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { name, email, business, phone, inquiry } = req.body;
