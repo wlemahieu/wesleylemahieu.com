1df7357275cc874a32adf865038fcc3140807db4
Wesley LeMahieu <wesleylemahieu@gmail.com>
2023-03-18 18:35:15 -0700

    fix sendEmail return status

--- functions/src/sendEmail.ts
+++ functions/src/sendEmail.ts
@@ -1,29 +1,26 @@
-import * as functions from "firebase-functions";
-import doSendEmail from "./helpers/sendEmail";
+import * as functions from 'firebase-functions';
+import doSendEmail from './helpers/sendEmail';
 
 const sendEmail = functions
-  .region("us-central1")
+  .region('us-central1')
   .runWith({
-    secrets: ["SENDGRID_API_KEY"],
-    memory: "256MB",
+    secrets: ['SENDGRID_API_KEY'],
+    memory: '256MB',
   })
   .https.onRequest(async (req, res) => {
     const { from, to, subject, html } = req.body;
 
     if (!from) {
-      throw new functions.https.HttpsError("internal", "No FROM was provided.");
+      throw new functions.https.HttpsError('internal', 'No FROM was provided.');
     }
     if (!to) {
-      throw new functions.https.HttpsError("internal", "No TO was provided.");
+      throw new functions.https.HttpsError('internal', 'No TO was provided.');
     }
     if (!subject) {
-      throw new functions.https.HttpsError(
-        "internal",
-        "No SUBJECT was provided."
-      );
+      throw new functions.https.HttpsError('internal', 'No SUBJECT was provided.');
     }
     if (!html) {
-      throw new functions.https.HttpsError("internal", "No HTML was provided.");
+      throw new functions.https.HttpsError('internal', 'No HTML was provided.');
     }
 
     // build email payload
@@ -31,7 +28,7 @@ const sendEmail = functions
 
     await doSendEmail(emailPayload);
 
-    res.status(500).send(true);
+    res.status(200).send(true);
   });
 
 export default sendEmail;
